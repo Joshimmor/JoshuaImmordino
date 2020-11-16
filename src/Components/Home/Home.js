158940a53bd0269d51476881e0d2b0136e9630fa
Joshua Immordino <joshimmor@Gmail.com>
2020-11-16 12:28:49 -0500

    change of SVG image from two to one singular planet

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,7 @@ import React  from 'react'
 import './Home.css';
 //import EmailForm from "./Form/EmailForm.js"
 import { useSpring, animated} from "react-spring";
-import ball from "../../assets/ball.svg"
-import rings from "../../assets/rings.svg";
+import ball from "../../assets/bglogo.svg";
 import Naviagtion from "../Navigation/Navigation";
 
 
@@ -11,32 +10,21 @@ export default function Home({darkMode, darkModeFunc}) {
 
     const positionEntry = useSpring({
             from:
-                {transform:"translate3d(0,100%,0)"},
+                {transform:"translate3d(100%,-100%,0) scale(0) rotate(0deg)"},
 
             to:
-                { transform:"translate3d(0,0,0)"},
+                { transform:"translate3d(0,0,0) scale(1) rotate(-45deg)"},
                 delay:700
                      
     })
-    const ringEntry = useSpring({
-        from:
-            {
-                transform:"translate3d(100%,-100%,0px) scale(1) rotate(0deg)",
-            },
+          
 
-        to:
-        {
-            transform:"translate3d(0,0,0) scale(1) rotate(-45deg)",
-        },
-            delay:1700
-                 
-})
    
     return (
         <React.Fragment>
              <div  className="home-container">
                 <animated.img style={positionEntry} src={ball} className="ball"/>
-                <animated.img style={ringEntry} src={rings} className="rings"/>
+                
              </div>
              <section className="software-container">
 
